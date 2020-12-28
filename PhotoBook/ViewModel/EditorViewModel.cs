dfc83c4b903cb1bf4a190acdca15b7a7dab5fc56
Aleksander Szulc <szulcalek@gmail.com>
2020-12-28 18:43:47 +0100

    testing done

--- PhotoBook/ViewModel/EditorViewModel.cs
+++ PhotoBook/ViewModel/EditorViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using PhotoBook.Model.Exporters;
 using Page = PhotoBook.Model.Pages.Page;
 using PhotoBookModel = PhotoBook.Model.PhotoBook;
 
@@ -40,6 +41,81 @@ namespace PhotoBook.ViewModel
 
             NotifyNestedViewModels();
         }
+
+        public RelayCommand ExportToHtml => new RelayCommand(() => {
+            ToHtml ob = new ToHtml(model.NumOfContentPages);
+
+            string fileNameExporter(string path)
+            {
+                string[] tmp = path.Split('\\');
+                return tmp[tmp.Length - 1];
+            }
+
+            switch (model.FrontCover.Background)
+            {
+                case Model.Backgrounds.BackgroundColor bgColor:
+                    ob.CreateFrontCover(ToHtml.CssBackground(bgColor.R, bgColor.G, bgColor.B), model.FrontCover.Title);
+                    break;
+                case Model.Backgrounds.BackgroundImage bgImage:
+                    ob.CreateFrontCover(ToHtml.CssBackground(fileNameExporter(bgImage.Image.DisplayedPath)), model.FrontCover.Title);
+                    break;
+            }
+
+            for (int i = 0; i < model.NumOfContentPages; i++)
+            {
+                ToHtml.Page left = new ToHtml.Page(ToHtml.CssBackground(0, 0, 0));
+
+                switch (model.GetContentPagesAt(i).Item1.Background)
+                {
+                    case Model.Backgrounds.BackgroundColor bgColor:
+                        left = new ToHtml.Page(ToHtml.CssBackground(bgColor.R, bgColor.G, bgColor.B));
+                        break;
+                    case Model.Backgrounds.BackgroundImage bgImage:
+                        left = new ToHtml.Page(ToHtml.CssBackground(fileNameExporter(bgImage.Image.DisplayedPath)));
+                        break;
+                }
+
+                for (int j = 0; j < model.GetContentPagesAt(i).Item1.Layout.NumOfImages; j++)
+                {
+                    left.AddPhotoWithDescription(
+                        fileNameExporter(model.GetContentPagesAt(i).Item1.GetImage(j).DisplayedPath),
+                        model.GetContentPagesAt(i).Item1.GetComment(j)
+                        );
+                }
+
+                ToHtml.Page right = new ToHtml.Page(ToHtml.CssBackground(0, 0, 0));
+
+                switch (model.GetContentPagesAt(i).Item2.Background)
+                {
+                    case Model.Backgrounds.BackgroundColor bgColor:
+                        right = new ToHtml.Page(ToHtml.CssBackground(bgColor.R, bgColor.G, bgColor.B));
+                        break;
+                    case Model.Backgrounds.BackgroundImage bgImage:
+                        right = new ToHtml.Page(ToHtml.CssBackground(fileNameExporter(bgImage.Image.DisplayedPath)));
+                        break;
+                }
+
+                for (int j = 0; j < model.GetContentPagesAt(i).Item2.Layout.NumOfImages; j++)
+                {
+                    right.AddPhotoWithDescription(
+                        fileNameExporter(model.GetContentPagesAt(i).Item2.GetImage(j).DisplayedPath),
+                        model.GetContentPagesAt(i).Item2.GetComment(j)
+                        );
+                }
+
+                ob.AddBookPages(left, right);
+            }
+
+            switch (model.BackCover.Background)
+            {
+                case Model.Backgrounds.BackgroundColor bgColor:
+                    ob.CreateBackCover(ToHtml.CssBackground(bgColor.R, bgColor.G, bgColor.B));
+                    break;
+                case Model.Backgrounds.BackgroundImage bgImage:
+                    ob.CreateBackCover(ToHtml.CssBackground(fileNameExporter(bgImage.Image.DisplayedPath)));
+                    break;
+            }
+        });
 
         // Page related commands
         public RelayCommand NextPage => new RelayCommand(() =>
