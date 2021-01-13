db63492df0232b9b0aed7d12917a4596e8b3774a
justcosmic <bartlomiej345@gmail.com>
2021-01-13 13:54:47 +0100

    Add DialogOK

---
+++ PhotoBook/ViewModel/Dialogs/DialogOKViewModel.cs
@@ -0,0 +1,17 @@
+﻿using GalaSoft.MvvmLight.Command;
+
+namespace PhotoBook.ViewModel.Dialogs
+{
+    public class DialogOKViewModel : DialogViewModelBase
+    {
+        public string Message { get; private set; }
+
+        public DialogOKViewModel(string message = "")
+        {
+            Message = message;
+        }
+
+        private RelayCommand okCommand = null;
+        public RelayCommand OKCommand => okCommand ?? (okCommand = new RelayCommand(Close));
+    }
+}
